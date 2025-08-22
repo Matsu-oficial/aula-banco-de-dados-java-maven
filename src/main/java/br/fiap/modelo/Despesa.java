cb8b636e3dc1d827fa81dde4d6555f374c2b7b8c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-21 21:55:18 -0300

    Continuação da aula de banco de dados + java + maven - 21/08/2025

---
+++ src/main/java/br/fiap/modelo/Despesa.java
@@ -0,0 +1,14 @@
+package br.fiap.modelo;
+
+import java.time.LocalDate;
+
+public class Despesa {
+    private Double valor;
+    private LocalDate data;
+    private Long id;
+    private String descricao;
+    private Categoria categoria;
+
+
+
+}
